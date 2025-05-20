db87ef9aa196b78ba089af3958d60e30f43985a7
Pavlo Stepchenko <pstep4e23@gmail.com>
2025-05-20 12:16:22 +0300

    add dotenv

--- taskTracker/settings.py
+++ taskTracker/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%+bzo9!wai)^bky_gozraw^y7wx8)c-loea)t0n6-$1_#6))58'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
